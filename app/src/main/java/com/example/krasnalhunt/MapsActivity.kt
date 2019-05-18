23a5deb27d66548e88e323ef3af41fbe2aad1904
Jakub Dąbek <jakub.dabek@gmail.com>
2019-05-18 20:16:03 +0200

    Use AndroidX instead of the Support Library

--- app/src/main/java/com/example/krasnalhunt/MapsActivity.kt
+++ app/src/main/java/com/example/krasnalhunt/MapsActivity.kt
@@ -4,10 +4,10 @@ import android.Manifest
 import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.location.Location
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
