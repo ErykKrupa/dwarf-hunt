c62195ef73f3edfe203ae6dba02b1625ef9e74d4
Kamil <kamil55525@gmail.com>
2019-05-24 00:29:28 +0200

    Optimize import and fix one file extension

--- app/src/main/java/com/example/krasnalhunt/DwarfViewFragment.kt
+++ app/src/main/java/com/example/krasnalhunt/DwarfViewFragment.kt
@@ -3,10 +3,10 @@ package com.example.krasnalhunt
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.krasnalhunt.model.DwarfItem
 import kotlinx.android.synthetic.main.fragment_dwarf_view.view.*
 
