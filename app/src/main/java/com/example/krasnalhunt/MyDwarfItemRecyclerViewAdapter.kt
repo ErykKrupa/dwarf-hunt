97d9575cb1cde71e2f517f58deee4f058e009096
Kamil <kamil55525@gmail.com>
2019-05-24 00:31:48 +0200

    Create dwarf item list view

--- app/src/main/java/com/example/krasnalhunt/MyDwarfItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/krasnalhunt/MyDwarfItemRecyclerViewAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.krasnalhunt
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.krasnalhunt.model.DwarfItem
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_dwarfitem.view.*
 
 
@@ -38,9 +38,28 @@ class MyDwarfItemRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = ald.currentList[position]
-        holder.mIdView.text = item.id.toString()
-        holder.mContentView.text = "${item.name} - ${item.location}"
-        holder.mView.background = if (item.caught) ColorDrawable(Color.GREEN) else ColorDrawable(Color.RED)
+        holder.nameHolder.text = item.name
+        holder.locationHolder.text = item.location
+        val imgResId = holder.imageHolder.resources.getIdentifier(
+            item.fileName.dropLast(4),
+            "drawable",
+            holder.imageHolder.context.packageName
+        )
+        if (imgResId == 0) {
+            Picasso.get().load(R.drawable.ic_block_black_24dp).into(holder.imageHolder)
+        } else {
+            Picasso.get().load(imgResId).error(R.drawable.ic_block_black_24dp).into(holder.imageHolder)
+        }
+        if (item.caught) {
+            holder.caughtImageHolder.setImageResource(R.drawable.ic_check_icon)
+            holder.caughtImageBackground.background =
+                holder.caughtImageBackground.resources.getDrawable(R.color.greenBackground, null)
+        } else {
+            holder.caughtImageHolder.setImageResource(R.drawable.ic_check_icon_gray)
+            holder.caughtImageBackground.background =
+                holder.caughtImageBackground.resources.getDrawable(R.color.grayBackground, null)
+        }
+        holder.distanceHolder.text = "0"
 
         with(holder.mView) {
             tag = item
@@ -66,11 +85,15 @@ class MyDwarfItemRecyclerViewAdapter(
     override fun getItemCount(): Int = ald.currentList.size
 
     inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
-        val mIdView: TextView = mView.item_number
-        val mContentView: TextView = mView.content
+        val nameHolder: TextView = mView.nameTextView
+        val locationHolder: TextView = mView.locationTextView
+        val imageHolder: ImageView = mView.dwarfImage
+        val caughtImageHolder: ImageView = mView.caughtImage
+        val caughtImageBackground: ImageView = mView.imageBackground
+        val distanceHolder: TextView = mView.distanceTextView
 
         override fun toString(): String {
-            return super.toString() + " '" + mContentView.text + "'"
+            return super.toString() + " '" + nameHolder.text + "'"
         }
     }
 }
