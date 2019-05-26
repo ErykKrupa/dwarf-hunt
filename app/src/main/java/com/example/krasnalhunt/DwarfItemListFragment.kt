55630bcf581025b5102f262baa88140d05eaf7dd
Kamil <kamil55525@gmail.com>
2019-05-26 13:53:10 +0200

    Remove toast from list fragment and change colors of application

--- app/src/main/java/com/example/krasnalhunt/DwarfItemListFragment.kt
+++ app/src/main/java/com/example/krasnalhunt/DwarfItemListFragment.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
@@ -25,7 +24,6 @@ class DwarfItemListFragment : Fragment(), MyDwarfItemRecyclerViewAdapter.OnListF
 
     override fun onListFragmentInteraction(item: DwarfItem?) {
         Log.d("TAG", "$item clicked")
-        Toast.makeText(context, "${item?.name}", Toast.LENGTH_LONG).show()
 
         if (item == null)
             return
